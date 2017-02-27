ec07fcd8aa10dd2ec0dde827f3fd633c11980955
shassaninia <xai.soft80@gmail.com>
2017-02-27 10:27:50 -0500

    console, setTime, setInterval

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+//The following are some of the functions on the node 'global' object
 console.log('hey ninjas');
+
+setTimeout(function(){
+    console.log('3 seconds have passed')
+},3000);
+
+var time = 0;
+setInterval(function(){
+    time += 2;
+    console.log(time + ' seconds have passed');
+},2000);
