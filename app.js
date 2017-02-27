38c83d788cada029c6d60ee59058430e2d43217f
shassaninia <xai.soft80@gmail.com>
2017-02-27 10:31:20 -0500

    __dirname and __filename

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 //The following are some of the functions on the node 'global' object
 
-var time = 0;
-var timer = setInterval(function(){
-    time += 2;
-    console.log(time + ' seconds have passed');
-    if(time > 5){
-        clearInterval(timer);
-    }
-},2000);
+console.log(__dirname);
+console.log(__filename);
