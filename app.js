7cfe1037310ce619ef772693bc923959b8564609
shassaninia <xai.soft80@gmail.com>
2017-02-27 10:28:58 -0500

    clearInteval

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 //The following are some of the functions on the node 'global' object
-console.log('hey ninjas');
-
-setTimeout(function(){
-    console.log('3 seconds have passed')
-},3000);
 
 var time = 0;
-setInterval(function(){
+var timer = setInterval(function(){
     time += 2;
     console.log(time + ' seconds have passed');
+    if(time > 5){
+        clearInterval(timer);
+    }
 },2000);
