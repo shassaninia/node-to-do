f348cd96b05f88cb0d4cb5c9dfc05b315e9302b9
shassaninia <xai.soft80@gmail.com>
2017-02-27 11:03:34 -0500

    Another way to export using object literal

--- stuff.js
+++ stuff.js
@@ -1,10 +1,16 @@
-module.exports.counter = function(arr){
+var counter = function(arr){
     return 'There are ' + arr.length + ' elements in this array';
 };
 
- module.exports.adder = function(a,b){
+var adder = function(a,b){
     return `The sum of the 2 numbers is ${a + b}`;
 }
 
- module.exports.pi = 3.142;
+var pi = 3.142;
+
+module.exports = {
+    counter:counter,
+    adder:adder,
+    pi:pi
+};
 
