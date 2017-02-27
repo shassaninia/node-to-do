df25dff88f0c1d299993a43b45a1de415d17a372
shassaninia <xai.soft80@gmail.com>
2017-02-27 17:50:06 -0500

    write async

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var fs = require('fs');
 
 //Will not block other code
 var readme = fs.readFile('readme.txt', 'utf8', function(error,data){
-    console.log(data);
+    fs.writeFile('writeme.txt', data);
 });
 
-console.log('this will show before the data from the file because the file read is async')
 
 
