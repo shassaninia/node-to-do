105b32e12df72258645a29569a543160f72dbf2f
shassaninia <xai.soft80@gmail.com>
2017-02-27 11:22:26 -0500

    reading from a file

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-var events = require('events');
-var util = require('util');
+var fs = require('fs');
 
-var Person = function(name){
-    this.name = name;
-};
+//Read file synchronously, this will read entire file before running more code
+//first parameter is file to read, second is character encoding of file
+var readme = fs.readFileSync('readme.txt', 'utf8');
 
-//Anything created using Person constructor can now have a custom event attached to it.
-util.inherits(Person,events.EventEmitter)
+console.log(readme);
 
-var john = new Person('john');
-var jane = new Person('jane');
-var doe = new Person('doe');
-
-var people = [john,jane,doe];
-
-//Attach a speak event to each person object
-people.forEach(function(person){
-
-    person.on('speak',function(message){
-        console.log(person.name + ' said: ' + message);
-    })    
-
-});
-
-john.emit('speak','I am john');
-
-jane.emit('speak','I am jane');
-
-doe.emit('speak','I am doe');
