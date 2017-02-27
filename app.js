cc5baad22b1e891e6e64e585e8ad1bfe8d0cc219
shassaninia <xai.soft80@gmail.com>
2017-02-27 11:17:15 -0500

    another event emitter example

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var events = require('events');
+var util = require('util');
 
-//create a custom event emitter
-var myEmitter = new events.EventEmitter();
+var Person = function(name){
+    this.name = name;
+};
 
-//Listen for 'someEvent'
-myEmitter.on('someEvent', function(message){
-    console.log(message);
+//Anything created using Person constructor can now have a custom event attached to it.
+util.inherits(Person,events.EventEmitter)
+
+var john = new Person('john');
+var jane = new Person('jane');
+var doe = new Person('doe');
+
+var people = [john,jane,doe];
+
+//Attach a speak event to each person object
+people.forEach(function(person){
+
+    person.on('speak',function(message){
+        console.log(person.name + ' said: ' + message);
+    })    
+
 });
 
-//Manually emit 'someEvent' with the data as the second parameter
-myEmitter.emit('someEvent','the event was emitted');
+john.emit('speak','I am john');
+
+jane.emit('speak','I am jane');
+
+doe.emit('speak','I am doe');
