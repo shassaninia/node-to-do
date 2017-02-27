3c4f782c504eab8a3ed514a61ffa55f2bf0a2054
shassaninia <xai.soft80@gmail.com>
2017-02-27 11:02:12 -0500

    another way to export

--- stuff.js
+++ stuff.js
@@ -1,15 +1,10 @@
-var counter = function(arr){
+module.exports.counter = function(arr){
     return 'There are ' + arr.length + ' elements in this array';
 };
 
-var adder = function(a,b){
+ module.exports.adder = function(a,b){
     return `The sum of the 2 numbers is ${a + b}`;
 }
 
-var pi = 3.142;
-
-
-   module.exports.counter = counter; //export counter function
-    module.exports.adder = adder; //export adder function
-    module.exports.pi = pi;
+ module.exports.pi = 3.142;
 
