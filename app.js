00957a6da321689be3e388de3e80788c4f842ac9
shassaninia <xai.soft80@gmail.com>
2017-02-28 14:38:33 -0500

    create read stream and listen for data event

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 
+//Create read stream to read 'readme.txt'
+//Data will be read in chunks (buffered)
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt');
+
+//data event is fired when our buffer is full
+myReadStream.on('data',function(chunk){
+    console.log('new chunk received:');
+    console.log(chunk);
+});
+
+
+
+/*
 var server = http.createServer(function(req,res){
     console.log('request was made: ' + req.url);
     res.writeHead(200,{'Content-Type':'text/plain'});
@@ -9,6 +23,7 @@ var server = http.createServer(function(req,res){
 
 server.listen(3000,'127.0.0.1');
 console.log('Listening on port 3000');
+*/
 
 
 
