0b129d157dbeac2ddbb9553dd41b1bc2fce0086b
shassaninia <xai.soft80@gmail.com>
2017-02-28 10:04:07 -0500

    logging request url

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req,res){
-
+    console.log('request was made: ' + req.url);
     res.writeHead(200,{'Content-Type':'text/plain'});
     res.end('Hey, this is a response from the server...');
 
