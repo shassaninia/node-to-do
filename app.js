74ddb6452d6331bf2d80bded2c01bbb3adb1d3be
shassaninia <xai.soft80@gmail.com>
2017-02-28 07:56:10 -0500

    make directory async

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var fs = require('fs');
 
-//remove direcory synchronously
-fs.rmdirSync('stuff');
+//create direcory Asynchronously
+fs.mkdir('stuff', function(){
+    fs.readFile('readme.txt','utf8',function(error,data){
+        fs.writeFile('./stuff/writeme.txt',data);
+    })
+});
 
 
 
