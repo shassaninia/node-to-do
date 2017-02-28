83620d96adeb311e4b61212fa7aa4a26c492f9b8
shassaninia <xai.soft80@gmail.com>
2017-02-28 10:01:11 -0500

    created basic node server

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var fs = require('fs');
+var http = require('http');
 
-//remove direcory Asynchronously (directory must exist and be empty)
-fs.rmdir('stuff',function(){
-    console.log('Directory removed');
+var server = http.createServer(function(req,res){
+
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hey, this is a response from the server...');
+
 });
+
+server.listen(3000,'127.0.0.1');
+console.log('Listening on port 3000');
+
 
 
