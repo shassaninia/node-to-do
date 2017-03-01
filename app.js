5ab9929efdba8c3425fcc783f9f892ab4fa68b27
shassaninia <xai.soft80@gmail.com>
2017-02-28 21:29:33 -0500

    template engines - ejs

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var express = require('express');
 // require('express')(); does the same thing
 var app = express();
 
+//tell express what view engine we want to use
+//ejs in this case
+app.set('view engine','ejs');
+
 //get request
 //express has extended the request and response
 //and added additional functionality to them
@@ -16,9 +20,11 @@ app.get('/contact',function(req,res){
      res.sendFile(__dirname + '/contact.html');
 });
 
-//:id is dynamic
-app.get('/profile/:id', function(req, res){
-    res.send('You requested to see a profile with the id of ' + req.params.id);
+//:name is dynamic
+app.get('/profile/:name', function(req, res){
+   
+   var data = {age:29, job:'developer'};
+   res.render('profile', {person: req.params.name, data: data});
 });
 
 //listen on port 3000
